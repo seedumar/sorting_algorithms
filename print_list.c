ff35c9cb6a6a61552dae030d582b57a0c7ab9398
seedumar <seedumar1999@gmail.com>
2023-04-25 07:35:03 -0700

    Sorting algorithms

---
+++ print_list.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+/**
+ * print_list - List of integers
+ * @list: List to be printed
+ */
+void print_list(const listint_t *list)
+{
+	int i;
+
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
+}
