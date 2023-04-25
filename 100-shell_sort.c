0a4cc1b9255fec2b45ee8cd582a9694378c21013
seedumar <seedumar1999@gmail.com>
2023-04-25 06:30:36 -0700

    Sorting algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -3,38 +3,43 @@
 /**
  * swap_int - Swap integers in an array.
  * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * @k: The second integer to swap.
  */
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *k)
 {
 	int tmp;
 
 	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	*a = *k;
+	*k = tmp;
 }
 
 /**
- * shell_sort - Sorts an array of integers in ascending
- * order using the Shell sort algorithm
+ * shell_sort - Sorts an array of integers
  * @size: size of the array
  * @array: An array of ints
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, index = 0;
+	size_t gap, i, j;
 
 	if (array == NULL || size < 2)
 		return;
-	while (gap < size / 3)
-		gap = 3 * gap + 1;
-	while (gap >= 1)
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
-			for (index = i; index >= gap &&
-			 (array[index] < array[index - gap]); index -= gap)
-				swap(array, index, index - gap);
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
 		print_array(array, size);
-		gap /= 3;
 	}
 }
